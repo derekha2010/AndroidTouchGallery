23d37753e2fd537f46c7ad630d2036cb297c68ca
Derek Ha <derek.ha@innopage.com>
2012-10-10 12:17:36 +0800

    use ViewPager.LayoutParams

--- src/ru/truba/touchgallery/GalleryWidget/LocalPagerAdapter.java
+++ src/ru/truba/touchgallery/GalleryWidget/LocalPagerAdapter.java
@@ -1,20 +1,3 @@
-/*
- Copyright (c) 2012 Roman Truba
-
- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- documentation files (the "Software"), to deal in the Software without restriction, including without limitation
- the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all copies or substantial
- portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
- TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
- THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
 package ru.truba.touchgallery.GalleryWidget;
 
 import android.content.Context;
@@ -23,18 +6,11 @@ import android.graphics.BitmapFactory;
 import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.LayoutParams;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Gallery;
-import ru.truba.touchgallery.R;
 import ru.truba.touchgallery.TouchView.PZSImageView;
 
-import java.util.List;
-
-/**
- * Class wraps URLs to adapter, then it instantiates <b>UrlTouchImageView</b>
- * objects to paging up through them.
- */
 public class LocalPagerAdapter extends PagerAdapter {
 
 	private int mImages[];
@@ -46,7 +22,6 @@ public class LocalPagerAdapter extends PagerAdapter {
 	}
 
 	public LocalPagerAdapter(Context context, int[] images) {
-		// TODO Auto-generated constructor stub
 		this.mContext = context;
 		mImages = images;
 	}
@@ -63,9 +38,10 @@ public class LocalPagerAdapter extends PagerAdapter {
 		Bitmap b = BitmapFactory.decodeResource(mContext.getResources(),
 				mImages[position]);
 		iv.setImageBitmap(b);
-		iv.setLayoutParams(new Gallery.LayoutParams(
-				ViewGroup.LayoutParams.FILL_PARENT,
-				ViewGroup.LayoutParams.FILL_PARENT));
+		LayoutParams p = new LayoutParams();
+		p.width = LayoutParams.MATCH_PARENT;
+		p.height = LayoutParams.MATCH_PARENT;
+		iv.setLayoutParams(p);
 
 		((ViewPager) collection).addView(iv, 0);
 		return iv;
