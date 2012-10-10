b84f6c3c9d8e730c65c90dcfa2ea433f0fa708ef
Derek Ha <derek.ha@innopage.com>
2012-10-10 11:42:03 +0800

    modify to use pzsimageview

---
+++ example/src/ru/truba/touchgallery/GalleryWidget/UrlPagerAdapter.java
@@ -0,0 +1,92 @@
+/*
+ Copyright (c) 2012 Roman Truba
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+ the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial
+ portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package ru.truba.touchgallery.GalleryWidget;
+
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Gallery;
+import ru.truba.touchgallery.TouchView.PZSImageView;
+import ru.truba.touchgallery.TouchView.UrlTouchImageView;
+
+import java.util.List;
+
+/**
+ Class wraps URLs to adapter, then it instantiates <b>UrlTouchImageView</b> objects to paging up through them.
+ */
+public class UrlPagerAdapter extends PagerAdapter {
+
+    private List<String> mResources;
+    private Context mContext;
+    public UrlPagerAdapter(Context context, List<String> resources){
+        this.mResources = resources;
+        this.mContext = context;
+    }
+
+    @Override
+    public void setPrimaryItem(ViewGroup container, int position, Object object) {
+        super.setPrimaryItem(container, position, object);
+        ((GalleryViewPager)container).mCurrentView = ((UrlTouchImageView)object).getImageView();
+    }
+
+    @Override
+    public Object instantiateItem(View collection, int position){
+        UrlTouchImageView iv = new UrlTouchImageView(mContext);
+        iv.setUrl(mResources.get(position));
+        iv.setLayoutParams(new Gallery.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+
+        ((ViewPager) collection).addView(iv, 0);
+        return iv;
+    }
+
+    @Override
+    public void destroyItem(View collection, int position, Object view){
+        ((ViewPager) collection).removeView((View) view);
+    }
+
+    @Override
+    public int getCount(){
+        return mResources.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object){
+        return view.equals(object);
+    }
+
+    @Override
+    public void finishUpdate(View arg0){
+    }
+
+    @Override
+    public void restoreState(Parcelable arg0, ClassLoader arg1){
+    }
+
+    @Override
+    public Parcelable saveState(){
+        return null;
+    }
+
+    @Override
+    public void startUpdate(View arg0){
+    }
+
+}
